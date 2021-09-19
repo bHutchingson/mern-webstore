e4dcc2f6ae7082735392bc52fb0cb86c9ae570d3
bHutchingson <69726199+bHutchingson@users.noreply.github.com>
2021-09-19 15:50:24 -0400

    created function to shorten product description when over 30 characters

--- client/src/components/productCard/index.js
+++ client/src/components/productCard/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const ProductCard = (props) => {
+  const truncateDesc = (str) => {
+    return str.length > 30 ? str.substring(0, 100) + "..." : str;
+  }
+
+  const truncateTitle = (str) => {
+    return str.length > 20 ? str.substring(0, 22) + "..." : str;
+  }
+
   return (
     <div className="antialiased bg-gray-200 text-gray-900 font-sans p-6">
       <div class="container mx-auto">
@@ -12,13 +20,13 @@ const ProductCard = (props) => {
               </div>
               <div class="p-4">
                 <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{props.category}</span>
-                <h2 class="mt-2 mb-2  font-bold">{props.title}</h2>
-                <p class="text-sm">{props.description}</p>
+                <h2 class="mt-2 mb-2  font-bold">{truncateTitle(props.title)}</h2>
+                <p class="text-sm">{truncateDesc(props.description)}</p>
                 <div class="mt-3 flex items-center">
                   <span class="text-sm font-semibold">$</span>&nbsp;<span class="font-bold text-xl">{props.price}</span>&nbsp;
                 </div>
               </div>
-              <div class="p-4 border-t border-b text-xs text-gray-700">
+              {/* <div class="p-4 border-t border-b text-xs text-gray-700">
                 <span class="flex items-center mb-1">
                   <i class="far fa-clock fa-fw mr-2 text-gray-900"></i>
                 </span>
@@ -26,7 +34,7 @@ const ProductCard = (props) => {
                   <i class="far fa-address-card fa-fw text-gray-900 mr-2"></i>
                 </span>
               </div>
-              <div class="p-4 flex items-center text-sm text-gray-600"><span class="ml-2"></span></div>
+              <div class="p-4 flex items-center text-sm text-gray-600"><span class="ml-2"></span></div> */}
             </a>
           </div>
         </div>
